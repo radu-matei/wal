572c34da0a028d6c01aff4e6f3b7bb9049d0b9e8
Radu M <root@radu.sh>
2019-12-09 11:59:22 -0800

    Evaluate infix and identifier expressions
    Signed-off-by: Radu M <root@radu.sh>

--- src/eval.rs
+++ src/eval.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::{Expression, Node, PrefixExpression, Program, Statement},
+    ast::{Expression, InfixExpression, Node, PrefixExpression, Program, Statement},
     lexer::Lexer,
     object::{Environment, Object},
     parser::Parser,
@@ -12,6 +12,11 @@ pub fn eval(node: Node, env: &mut Environment) -> Result<Object, EvaluatorError>
 
         Node::Statement(stmt) => match stmt {
             Statement::Expression(expr) => eval(Node::Expression(expr), env),
+            Statement::Let(ls) => {
+                let obj = eval(Node::Expression(ls.value), env)?;
+                env.set(ls.name, &obj);
+                Ok(obj)
+            },
             _ => Err(EvaluatorError::InvalidOperation(format!(
                 "cannot handle statement {}",
                 stmt
@@ -23,10 +28,13 @@ pub fn eval(node: Node, env: &mut Environment) -> Result<Object, EvaluatorError>
             Expression::Boolean(b) => Ok(Object::Boolean(b)),
             Expression::String(s) => Ok(Object::String(s)),
             Expression::Prefix(p) => eval_prefix_expression(p, env),
+            Expression::Infix(i) => eval_infix_expression(i, env),
+            Expression::Identifier(id) => eval_identifier(id, env),
             _ => Err(EvaluatorError::InvalidOperation(format!(
                 "cannot handle expression {}",
                 expr
-            ))),        },
+            ))),
+        },
     }
 }
 
@@ -56,20 +64,96 @@ fn eval_prefix_expression(
         },
         Token::MINUS => match right {
             Object::Integer(i) => Ok(Object::Integer(-i)),
-            _ => Err(EvaluatorError::InvalidOperation(
-                format! {"cannot negate non-numeric value {}", right},
-            )),
+            _ => Err(EvaluatorError::InvalidOperation(format!(
+                "cannot negate non-numeric value {}",
+                right
+            ))),
         },
 
-        x => Err(EvaluatorError::InvalidOperation(
-            format! {"cannot handle prefix operation {} {}", x, right},
-        )),
+        x => Err(EvaluatorError::InvalidOperation(format!(
+            "cannot handle prefix operation {} {}",
+            x, right
+        ))),
+    }
+}
+
+fn eval_infix_expression(
+    expr: InfixExpression,
+    env: &mut Environment,
+) -> Result<Object, EvaluatorError> {
+    let left = &eval(Node::Expression(*expr.left), env)?;
+    let right = &eval(Node::Expression(*expr.right), env)?;
+
+    match (left, right) {
+        (Object::Integer(l), Object::Integer(r)) => match expr.operator {
+            Token::PLUS | Token::MINUS | Token::ASTERISK | Token::SLASH => {
+                return Ok(Object::Integer(eval_infix_op(expr.operator, *l, *r)?))
+            }
+            Token::LT => return Ok(Object::Boolean(l < r)),
+            Token::GT => return Ok(Object::Boolean(l > r)),
+            Token::EQ => return Ok(Object::Boolean(l == r)),
+            Token::NE => return Ok(Object::Boolean(l != r)),
+            _ => {
+                return Err(EvaluatorError::InvalidOperation(format!(
+                    "cannot handle operation {} {} {}",
+                    l, expr.operator, r
+                )))
+            }
+        },
+        (Object::Boolean(l), Object::Boolean(r)) => match expr.operator {
+            Token::EQ => return Ok(Object::Boolean(l == r)),
+            Token::NE => return Ok(Object::Boolean(l != r)),
+            _ => {
+                return Err(EvaluatorError::InvalidOperation(format!(
+                    "cannot handle operation {} {} {}",
+                    l, expr.operator, r
+                )))
+            }
+        },
+        (Object::String(l), Object::String(r)) => match expr.operator {
+            Token::PLUS => return Ok(Object::String(String::from(format!("{}{}", l, r)))),
+            _ => {
+                return Err(EvaluatorError::InvalidOperation(format!(
+                    "cannot handle operation {} {} {}",
+                    l, expr.operator, r
+                )))
+            }
+        },
+
+        _ => {
+            return Err(EvaluatorError::InvalidOperation(format!(
+                "cannot handle operation {} {} {}",
+                left, expr.operator, right
+            )))
+        }
+    }
+}
+
+fn eval_identifier(id: String, env: &mut Environment) -> Result<Object, EvaluatorError> {
+    Ok(env
+        .get(&id)
+        .ok_or_else(|| EvaluatorError::UnknownIdentifier(id))?
+        .clone())
+}
+
+fn eval_infix_op(op: Token, l: i64, r: i64) -> Result<i64, EvaluatorError> {
+    match op {
+        Token::PLUS => Ok(l + r),
+        Token::MINUS => Ok(l - r),
+        Token::ASTERISK => Ok(l * r),
+        Token::SLASH => Ok(l / r),
+
+        _ => Err(EvaluatorError::InvalidOperation(format!(
+            "cannot execute {} {} {}",
+            l, op, r
+        ))),
     }
 }
 
 #[derive(Debug)]
 pub enum EvaluatorError {
     InvalidOperation(String),
+    UnknownIdentifier(String)
 }
 
 #[test]
@@ -79,13 +163,12 @@ fn evaluate_integer_expression() {
         ("10", 10),
         ("-5", -5),
         ("-10", -10),
-        // ("5 + 5 + 5 + 5 - 10", 10),
-        // ("2 * 2 * 2 *2 * 2", 32),
-        // ("-50 + 100 + -50", 0),
-        // ("50 / 2 * 2 + 10", 60),
-        // ("2 * (5 + 10)", 30),
-        // ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
-        // ("4 % 3", 1),
+        ("5 + 5 + 5 + 5 - 10", 10),
+        ("2 * 2 * 2 *2 * 2", 32),
+        ("-50 + 100 + -50", 0),
+        ("50 / 2 * 2 + 10", 60),
+        ("2 * (5 + 10)", 30),
+        ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
     ];
 
     for (input, want) in tests {
--- src/repl.rs
+++ src/repl.rs
@@ -4,6 +4,8 @@ use std::io::{stdin, stdout, Write};
 const PROMPT: &str = ">>";
 
 pub fn start() {
+    let mut env = object::Environment::new();
+
     loop {
         print!("{} ", PROMPT);
         let buffer = &mut String::new();
@@ -12,17 +14,16 @@ pub fn start() {
 
         let l = lexer::Lexer::new(buffer).unwrap();
         let mut p = parser::Parser::new(l).unwrap();
-        let mut env = object::Environment::new();
 
         match p.parse() {
             Ok(pr) => {
                 println!("{:?}", pr);
                 match eval::eval_program(pr, &mut env) {
                     Ok(obj) => println!("{}", obj),
-                    Err(err) => println!("{:?}", err),
+                    Err(err) => println!("evaluator error: {:?}", err),
                 };
             }
-            Err(err) => println!("{:?}", err),
+            Err(err) => println!("parser error: {:?}", err),
         }
 
         // loop {
